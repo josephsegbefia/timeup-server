a1641606ce89ae5ab9ca62799fc68694d6f73b45
Joseph Segbefia <elorma.segbefia@gmail.com>
2023-08-25 00:49:55 +0200

    Start user model

---
+++ models/User.model.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+const { Schema, model } = mongoose;
+
+const userSchema = new Schema({
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  email: { type: String, unique: true, required: true },
+  password: { type: String, required: true }
+});
+
+module.exports = model("User", userSchema);
