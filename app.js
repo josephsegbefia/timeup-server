ae81e0e22480be4090ea5854d4b3d89aa0ae9718
Joseph Segbefia <elorma.segbefia@gmail.com>
2023-08-25 14:03:25 +0200

    Add signup and login to backend

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ require("dotenv").config();
 // ℹ️ Connects to the database
 require("./db");
 
+const { isAuthenticated } = require("./middleware/jwt.middleware");
 // Handles http requests (express is node js framework)
 // https://www.npmjs.com/package/express
 const express = require("express");
@@ -17,6 +18,9 @@ require("./config")(app);
 // 👇 Start handling routes here
 const indexRoutes = require("./routes/index.routes");
 app.use("/api", indexRoutes);
+
+const authRouter = require("./routes/auth.routes");
+app.use("/auth", authRouter);
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
