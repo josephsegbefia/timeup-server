8efaff50b86ffb3e9939c081110b75136568f0a8
Joseph Segbefia <elorma.segbefia@gmail.com>
2023-08-29 00:54:24 +0200

    Add edit route to timer

--- routes/timer.routes.js
+++ routes/timer.routes.js
@@ -6,6 +6,7 @@ const User = require("../models/User.model");
 
 const router = express.Router();
 
+// Create a timer for a logged in user
 router.post("/timers", (req, res, next) => {
   const { user, title, project } = req.body;
 
@@ -18,9 +19,49 @@ router.post("/timers", (req, res, next) => {
         //   res.redirect("/timers");
       });
     })
-    .catch((error) =>
-      res.status(500).json({ message: "Internal Server Error" })
-    );
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json({ message: "Internal Server Error" });
+    });
+});
+
+//Get a user's timers
+router.get("/:user/timers", (req, res, next) => {
+  const { user } = req.params;
+  Timer.find({ user })
+    .then((timers) => {
+      res.status(200).json(timers);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json({ message: "Internal Server Error" });
+    });
+});
+
+//Edit a user's timers
+router.post("/:user/timers/:timerId/edit", async (req, res, next) => {
+  try {
+    const { user, timerId } = req.params;
+    const { title, project } = req.body;
+
+    const timer = await Timer.findOne({ _id: timerId, user });
+
+    // Verify a timers existence
+    if (!timer) {
+      return res.status(404).json({ message: "Timer does not exist" });
+    }
+
+    // Update the fields
+    timer.title = title;
+    timer.project = project;
+
+    //Save the timer
+    const updatedTimer = await timer.save();
+    res.status(200).json(updatedTimer);
+  } catch (error) {
+    console.log("Error updating the card:", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 });
 
 module.exports = router;
