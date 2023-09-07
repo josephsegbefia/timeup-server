8c9f9489b10c93a56c16f308b9bbb5588ad9e757
Joseph Segbefia <elorma.segbefia@gmail.com>
2023-09-08 01:23:32 +0200

    Improve error messages

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -122,7 +122,9 @@ router.post("/login", (req, res, next) => {
         });
         res.status(200).json({ authToken: authToken });
       } else {
-        res.status(401).json({ message: "Unable to authenticate the user" });
+        res.status(401).json({
+          message: "Unable to authenticate the user. Wrong email or password"
+        });
       }
     })
     .catch((err) => res.status(500).json({ message: "Internal Serber Error" }));
